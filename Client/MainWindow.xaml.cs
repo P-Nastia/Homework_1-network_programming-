a89296d1f31c4b2c49d930bfcb8bde063e6a60d1
P-Nastia <nastia.pyvovarchuck@gmail.com>
2025-01-11 15:57:46 +0200

    Homework_1 finished

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Security;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Socket socketServer;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(IPAddress.Parse(this.ipAddressTB.Text) != null && !String.IsNullOrEmpty(this.portTB.Text))
+            {
+                var ip = IPAddress.Parse(this.ipAddressTB.Text);
+                var port = int.Parse(this.portTB.Text);
+                try
+                {
+                    var serverEndPoint = new IPEndPoint(ip, port);
+
+                    socketServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    socketServer.Connect(serverEndPoint);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void sendMessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (!String.IsNullOrEmpty(this.messageTB.Text))
+            {
+                try
+                {
+                    this.answerTB.Text = "";
+                    var data = Encoding.Unicode.GetBytes(this.messageTB.Text);
+
+                    socketServer.Send(data);
+
+                    data = new byte[1024];
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = socketServer.Receive(data);
+                        this.answerTB.Text = Encoding.Unicode.GetString(data);
+
+                    } while (socketServer.Available > 0);
+                    this.messageTB.Clear();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq.Expressions;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+var localHost = await Dns.GetHostEntryAsync(Dns.GetHostName());
+int i = 0;
+foreach (var item in localHost.AddressList)
+{
+    Console.WriteLine($"{++i}.{item}");
+}
+
+
+Console.WriteLine("Обери IP-адресу: ->_");
+int numberIP = int.Parse(Console.ReadLine());
+Console.WriteLine("Обери порт: ->_");
+int serverPort = int.Parse(Console.ReadLine());
+Console.Title = localHost.AddressList[numberIP - 1].ToString() + $":{serverPort}";
+
+var serverIP = localHost.AddressList[numberIP - 1];
+
+IPEndPoint iPEndPoint = new IPEndPoint(serverIP, serverPort);
+Socket server = new Socket(AddressFamily.InterNetwork,
+    SocketType.Stream, ProtocolType.Tcp);
+
+try
+{
+    server.Bind(iPEndPoint);
+    server.Listen(10);
+    while (true)
+    {
+        Console.WriteLine("Сервер очікує");
+        Socket client = server.Accept();
+        Console.WriteLine($"Клієнт -> {client.RemoteEndPoint}");
+        int bytes = 0; 
+        byte[] buffer = new byte[1024];
+        do
+        {
+            bytes = client.Receive(buffer);
+            Console.WriteLine($"Повідомлення: '{Encoding.Unicode.GetString(buffer)}'");
+        } while (client.Available > 0);
+
+        buffer = Encoding.Unicode.GetBytes($"{DateTime.Now}");
+        client.Send(buffer);
+
+        client.Shutdown(SocketShutdown.Both);
+        client.Close();
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Проблема {ex.Message}");
+}
+
+Console.ReadKey();
